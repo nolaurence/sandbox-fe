d333846a3450a49dad5afef5b1f905cc425a8043
nolaurence <nolaurence@outlook.com>
2025-06-23 22:22:50 +0800

    change framework to umi - 1

--- sandbox-fe/services/request/type.d.ts
+++
@@ -1,9 +0,0 @@
-// request 方法 opts 参数的接口
-import type { AxiosRequestConfig } from 'axios';
-
-export interface RequestOptions extends AxiosRequestConfig {
-    skipErrorHandler?: boolean;
-    requestInterceptors?: IRequestInterceptorTuple[];
-    responseInterceptors?: IResponseInterceptorTuple[];
-    [key: string]: any;
-}
